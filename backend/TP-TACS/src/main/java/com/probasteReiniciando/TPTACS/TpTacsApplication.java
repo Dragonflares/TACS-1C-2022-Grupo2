9407a20fa01689681fbb835657b86204c0207325
mackhood <germanjugo@hotmail.com>
2022-04-02 12:28:35 -0300

    add backend stuff

---
+++ backend/TP-TACS/src/main/java/com/probasteReiniciando/TPTACS/TpTacsApplication.java
@@ -0,0 +1,15 @@
+package com.probasteReiniciando.TPTACS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class TpTacsApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TpTacsApplication.class, args);
+	}
+
+}
---
+++ backend/TP-TACS/src/test/java/com/probasteReiniciando/TPTACS/TpTacsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.probasteReiniciando.TPTACS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTacsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
