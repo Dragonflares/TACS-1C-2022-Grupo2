5b3a1811480d82c9d839fb570c2a5ae2557b5bed
mackhood <germanjugo@hotmail.com>
2022-04-07 18:54:14 -0300

    upload generation of jwt token working

--- backend/TP-TACS/src/main/java/com/probasteReiniciando/TPTACS/services/user/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.probasteReiniciando.TPTACS.services.user;
-
-import com.probasteReiniciando.TPTACS.dto.TokenDto;
-import com.probasteReiniciando.TPTACS.dto.request.LoginRequestDto;
-
-public interface IUserService {
-        TokenDto authenticate(LoginRequestDto loginRequestDTO);
-}
