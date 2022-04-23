236884f05260196453acc6d755263782fa79d632
SantiagoAspres <santi.aspres@gmail.com>
2022-04-23 20:49:55 -0300

    I tried to replace Popup with Modal

--- frontend/src/pages/login/popUp/PopUpCustom.js
+++ frontend/src/pages/login/popUp/PopUpCustom.js
@@ -1,40 +1,40 @@
-import React, {Component} from 'react'
+import React, {Component, useState } from 'react'
 import Popup from 'reactjs-popup'
+import { Modal } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+
+const [show, setShow] = useState(false);
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
 
 export class PopUpCustom extends Component {
 
 render(){
 
   return (
-    <Popup trigger={<Button variant="success" size="lg">Create new Account</Button>} modal>
-      <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-        </Form>
-        </Card.Body>
-      </Card>
-  </Popup>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 }
