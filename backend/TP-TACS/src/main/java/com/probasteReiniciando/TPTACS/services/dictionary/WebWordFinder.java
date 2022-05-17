6958c8f44db5bcb3690e6e87788437067995a22e
snouthill <dagostino@frba.utn.edu.ar>
2022-05-16 22:56:14 -0300

    Deleted unused imports @ WebWordFinder

--- backend/TP-TACS/src/main/java/com/probasteReiniciando/TPTACS/services/dictionary/WebWordFinder.java
+++ backend/TP-TACS/src/main/java/com/probasteReiniciando/TPTACS/services/dictionary/WebWordFinder.java
@@ -4,15 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @Component("webWordFinder")
