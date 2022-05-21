40f9075b59ae722313b7769ded7d6fa6db456846
Juan <jcarassai@frba.utn.edu.ar>
2022-05-21 16:38:42 -0300

    obtainPositions Controller test

--- backend/TP-TACS/src/main/java/com/probasteReiniciando/TPTACS/dto/PositionDto.java
+++ backend/TP-TACS/src/main/java/com/probasteReiniciando/TPTACS/dto/PositionDto.java
@@ -2,12 +2,14 @@ package com.probasteReiniciando.TPTACS.dto;
 
 import com.probasteReiniciando.TPTACS.dto.user.UserDto;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class PositionDto {
 
     private UserDto user;
