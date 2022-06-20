d2c3d23102fb9c1e628293850c103d35cc64375b
smchejolan <smchejolan@gmail.com>
2022-06-20 19:48:37 -0300

    useReducer in results

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useReducer} from 'react';
+import React, {useCallback, useReducer} from 'react';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button'
