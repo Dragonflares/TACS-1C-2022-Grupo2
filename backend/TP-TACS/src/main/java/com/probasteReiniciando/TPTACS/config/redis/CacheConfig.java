17a20546c4336422f4af006a9369e8e8e797b84b
mackhood <germanjugo@hotmail.com>
2022-07-08 14:50:04 -0300

    removed deprecated config

--- backend/TP-TACS/src/main/java/com/probasteReiniciando/TPTACS/config/redis/CacheConfig.java
+++ backend/TP-TACS/src/main/java/com/probasteReiniciando/TPTACS/config/redis/CacheConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -26,10 +27,9 @@ public class CacheConfig {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConFactory = new  JedisConnectionFactory();
-        jedisConFactory.setHostName("localhost");
-        jedisConFactory.setPort(6379);
-        return jedisConFactory;
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        return new JedisConnectionFactory(config);
     }
 
 
